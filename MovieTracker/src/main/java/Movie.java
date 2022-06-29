16f01f72f946075d374e9960a9d33fe6d9432f73
Chris Oswald <OzzieGitHub@users.noreply.github.com>
2022-06-29 01:48:54 -0400

    Started putting together the program outline and started filling out the Movie class.

---
+++ MovieTracker/src/main/java/Movie.java
@@ -0,0 +1,44 @@
+
+public class Movie {
+    private String title;
+    private String director;
+    private int yearReleased;
+    private boolean doIOwn;
+    private String typeOfMedia;
+    private boolean haveIWatched;
+    
+    public Movie(String title) {
+        this.title = title;
+        this.director = "unk";
+        this.yearReleased = 0;
+        this.doIOwn = false;
+        this.typeOfMedia = "unk";
+        this.haveIWatched = false;
+    }
+    public Movie(String title, String director) {
+        this.title = title;
+        this.director = director;
+        this.yearReleased = 0;
+        this.doIOwn = false;
+        this.typeOfMedia = "unk";
+        this.haveIWatched = false;
+    }
+    public Movie(String title, String director, int yearReleased) {
+        this.title = title;
+        this.director = director;
+        this.yearReleased = yearReleased;
+        this.doIOwn = false;
+        this.typeOfMedia = "unk";
+        this.haveIWatched = false;
+    }
+    public Movie(String title, String director, int yearReleased, boolean doIOwn, 
+            String typeOfMedia, boolean haveIWatched) {
+        this.title = title;
+        this.director = director;
+        this.yearReleased = yearReleased;
+        this.doIOwn = doIOwn;
+        this.typeOfMedia = typeOfMedia;
+        this.haveIWatched = haveIWatched;
+    }
+    
+}
